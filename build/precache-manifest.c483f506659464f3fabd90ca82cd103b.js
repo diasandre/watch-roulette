0b4d3422e68ca790d6b7b6b1821349e9277fe36d
andré <andredds12@gmail.com>
2020-04-18 22:15:50 -0300

    build

---
+++ build/precache-manifest.c483f506659464f3fabd90ca82cd103b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9bed625c498f3e12a549c562b90b099",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc8a99dd83ccdc67f032",
+    "url": "/static/css/main.ce9a3144.chunk.css"
+  },
+  {
+    "revision": "ca741b1e969634d57ad4",
+    "url": "/static/js/2.0beacc0a.chunk.js"
+  },
+  {
+    "revision": "e85e620f3fcc49b161ee1b95d282124d",
+    "url": "/static/js/2.0beacc0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc8a99dd83ccdc67f032",
+    "url": "/static/js/main.ca15941d.chunk.js"
+  },
+  {
+    "revision": "5256b7b09922785e57a8",
+    "url": "/static/js/runtime-main.73eaca36.js"
+  }
+]);
