9a31d4eb9d752970fa8a49bd189053a3eb729d23
andré <andredds12@gmail.com>
2020-08-08 13:50:36 -0300

    remove auth

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 import { TV_SHOWS, RANDOM } from "../constants/ApiRoutes";
 
-const AUTH = {
-  auth: {
-    username: process.env.REACT_APP_API_USERNAME,
-    password: process.env.REACT_APP_API_USERNAME,
-  },
-};
+// const AUTH = {
+//   auth: {
+//     username: process.env.REACT_APP_API_USERNAME,
+//     password: process.env.REACT_APP_API_USERNAME,
+//   },
+// };
 
 const API_BASE_URL = "https://wetwa-api.herokuapp.com";
 
 export const listAllTvShows = () =>
-  axios.get(`${API_BASE_URL}${TV_SHOWS.BASE_URL}`, AUTH);
+  axios.get(`${API_BASE_URL}${TV_SHOWS.BASE_URL}`);
 
 export const getRandom = (id) =>
-  axios.get(`${API_BASE_URL}${RANDOM.BASE_URL}/${id}`, AUTH);
+  axios.get(`${API_BASE_URL}${RANDOM.BASE_URL}/${id}`);
