a24f2a539619539dbcd3616c339a90e5402ae331
andré <andredds12@gmail.com>
2020-08-08 19:11:22 -0300

    update layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import React from 'react';
-import './style.css';
-import SearchBar from './components/SearchBar';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import "./style.css";
+import SearchBar from "./components/SearchBar";
+import Typography from "@material-ui/core/Typography";
+import { Paper } from "@material-ui/core";
+import logo from "./img/logo.png";
 
 function App() {
   return (
     <div className="main">
-      <header>
-        <Typography variant="h3" color="textPrimary" align="center">
-          which episode to watch again
-        </Typography>
-        <Typography variant="subtitle2" color="textSecondary" align="center">
-          random episodes from your favorite tv shows
-        </Typography>
-        <SearchBar />
-      </header>
+      <div>
+        <Paper variant="outlined" className="container">
+          <img src={logo} width="200" alt="logo" />
+          <header>
+            <Typography variant="h4" color="textPrimary" align="center">
+              which episode to watch again
+            </Typography>
+            <Typography
+              variant="subtitle2"
+              color="textSecondary"
+              align="center"
+            >
+              random episodes from your favorite tv shows
+            </Typography>
+            <SearchBar />
+          </header>
+        </Paper>
+        <footer>
+          <Typography variant="subtitle2" color="textPrimary" align="center">
+            made with <span className="heart">♥</span> by{" "}
+            <a href="https://github.com/diasandre">@diasandre</a>
+          </Typography>
+        </footer>
+      </div>
     </div>
   );
 }
