7d65b95cd87b88173e4196fe26b52aa794da7555
andré <andredds12@gmail.com>
2021-03-29 23:22:02 -0300

    change font, improve layout

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { theme } from "./theme";
+import { MuiThemeProvider } from "@material-ui/core";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 serviceWorker.unregister();
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { createMuiTheme } from "@material-ui/core";
+
+export const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      "Montserrat",
+      "sans-serif"
+    ].join(","),
+  },
+});
